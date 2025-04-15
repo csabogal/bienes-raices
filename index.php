88492f7e857ea7e0051de1e471477ea6aafc41de
Camilo Sabogal <camilo.sabogal@hotmail.com>
2025-04-15 06:27:32 -0500

    creando plantilla en PHP para el footer y agregando el año dinamico

--- index.php
+++ index.php
@@ -255,21 +255,4 @@ include 'includes/templates/header.php';
 </div>
 <!-- fin seccion inferior -->
 
-<footer class="footer seccion">
-    <div class="contenedor contenedor-footer">
-        <nav class="navegacion">
-            <a href="nosotros.php">Nosotros</a>
-            <a href="anuncios.php">Anuncios</a>
-            <a href="blog.php">Blog</a>
-            <a href="contacto.php">Contacto</a>
-        </nav>
-    </div>
-
-    <p class="copyright">Todos los derechos reservados 2025 &copy;</p>
-</footer>
-<!-- Fin footer -->
-
-<script src="build/js/bundle.min.js"></script>
-</body>
-
-</html>
+<?php include 'includes/templates/footer.php'; ?>
